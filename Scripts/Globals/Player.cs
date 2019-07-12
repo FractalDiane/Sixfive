76449265f5e059803f717b6f7a1be1751e943e12
Duncan Sparks <duncan.sprks@gmail.com>
2019-07-11 23:06:20 -0700

    Setup

---
+++ Scripts/Globals/Player.cs
@@ -0,0 +1,195 @@
+using Godot;
+using System;
+using System.Runtime.CompilerServices;
+
+public class Player : KinematicBody
+{
+	private Vector3 vel = new Vector3(0, 0, 0);
+	private float rotSpeed = 0f;
+	private int rotDir = 1;
+	private float targetAngle = 0f;
+
+	private Vector2 face = new Vector2(1, 1);
+	private bool walking = false;
+	private bool jumping = false;
+
+	private const float WALKSPEED = 1;
+	private const float GRAVITY = -0.2f;
+	private const float JUMP_FORCE = 3.5f;
+
+	private int sound = -1;
+
+	private int sightStartX;
+	private int sightStartY;
+
+	public enum ST { Move, NoInput };
+	private ST state = ST.Move;
+
+	// Refs
+	private AnimatedSprite3D spr;
+	private CollisionShape sight;
+	private Camera camera;
+
+	// ================================================================
+	
+	public override void _Ready()
+	{
+		spr = GetNode<AnimatedSprite3D>("Sprite");
+		sight = GetNode<CollisionShape>("CollisionShape");
+		camera = GetTree().GetRoot().GetNode<Spatial>("Scene").GetNode<Camera>("Camera");
+	}
+
+
+	public override void _PhysicsProcess(float delta)
+	{
+		switch (state)
+		{
+			case ST.Move:
+			{
+				Movement(delta);
+				walking = vel.x != 0 || vel.z != 0;
+				break;
+			}
+		}
+
+		jumping = !IsOnFloor();
+		vel.y += GRAVITY * delta * 60f;
+		spr.RotationDegrees = new Vector3(spr.RotationDegrees.x, Mathf.Clamp(spr.RotationDegrees.y + rotSpeed * Mathf.Abs(spr.RotationDegrees.y - targetAngle) * rotDir, 0, 179), spr.RotationDegrees.z);
+	
+		SpriteAnimation();
+		SightMovement();
+		vel = MoveAndSlide(vel * WALKSPEED, new Vector3(0, 1, 0));
+
+		// add if here
+		CamreaMovement();
+	}
+
+	// ================================================================
+
+	public void Stop()
+	{
+		state = ST.NoInput;
+		vel.x = 0;
+		vel.z = 0;
+		walking = false;
+	}
+
+	// ================================================================
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void Movement(float delta)
+	{
+		int x1 = Input.IsActionPressed("move_right") ? 1 : 0;
+		int x2 = Input.IsActionPressed("move_left") ? 1 : 0;
+		int z1 = Input.IsActionPressed("move_down") ? 1 : 0;
+		int z2 = Input.IsActionPressed("move_up") ? 1 : 0;
+
+		vel.x = x1 - x2;
+		vel.z = z1 - z2;
+
+		if (Input.IsActionJustPressed("move_up"))
+			face.y = -1;
+
+		if (Input.IsActionJustPressed("move_down"))
+			face.y = 1;
+
+		if (Input.IsActionJustPressed("move_left"))
+		{
+			if (face.x != -1)
+			{
+				rotSpeed = 0.07f * delta * 60;
+				rotDir = 1;
+				targetAngle = 179;
+				face.x = -1;
+			}
+
+			if (!Input.IsActionPressed("move_up"))
+				face.y = 1;
+		}
+
+		if (Input.IsActionJustPressed("move_right"))
+		{
+			if (face.x != 1)
+			{
+				rotSpeed = 0.07f * delta * 60;
+				rotDir = -1;
+				targetAngle = 0;
+				face.x = 1;
+			}
+
+			if (!Input.IsActionPressed("move_up"))
+				face.y = 1;
+		}
+
+		if (Input.IsActionJustPressed("move_jump") && IsOnFloor())
+		{
+			vel.y = JUMP_FORCE;
+			jumping = true;
+		}
+	}
+
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void SpriteAnimation()
+	{
+		switch (face.y)
+		{
+			case -1: // Facing up
+			{
+				if (face.x == -1 && spr.RotationDegrees.y > 90)
+				{
+					spr.Play(walking ? "ul_walk" : "ul");
+					if (jumping)
+						spr.Play("ul_jumpup");
+				}
+				else if (face.x == 1 && spr.RotationDegrees.y < 90)
+				{
+					spr.Play(walking ? "ur_walk" : "ur");
+					if (jumping)
+						spr.Play("ur_jumpup");
+				}
+
+				break;
+			}
+			case 1: // Facing down
+			{
+				if (face.x == -1 && spr.RotationDegrees.y > 90)
+				{
+					spr.Play(walking ? "dl_walk" : "dl");
+					if (jumping)
+						spr.Play(vel.y > 0 ? "dl_jumpup" : "dl_jumpdown");
+				}
+				else if (face.x == 1 && spr.RotationDegrees.y < 90)
+				{
+					spr.Play(walking ? "dr_walk" : "dr");
+					if (jumping)
+						spr.Play(vel.y > 0 ? "dr_jumpup" : "dr_jumpdown");
+				}
+
+				break;
+			}
+		}
+	}
+
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void SightMovement()
+	{
+		sight.Translation = new Vector3(sightStartX * face.x, sight.Translation.y, sight.Translation.z);
+	}
+
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private void CamreaMovement()
+	{
+		if (camera.Translation.x < Translation.x)
+			camera.Translation = new Vector3(Mathf.Min(camera.Translation.x + (0.05f * Mathf.Abs(Translation.x - camera.Translation.x)), Translation.x), camera.Translation.y, camera.Translation.z);
+		else if (camera.Translation.x > Translation.x)
+			camera.Translation = new Vector3(Mathf.Max(camera.Translation.x - (0.05f * Mathf.Abs(Translation.x - camera.Translation.x)), Translation.x), camera.Translation.y, camera.Translation.z);
+
+		if (camera.Translation.z < Translation.z + 1.5f)
+			camera.Translation = new Vector3(camera.Translation.x, camera.Translation.y, Mathf.Min(camera.Translation.z + (0.05f * Mathf.Abs(Translation.z + 1.5f - camera.Translation.z)), Translation.z + 1.5f));
+		else if (camera.Translation.z > Translation.z + 1.5f)
+			camera.Translation = new Vector3(camera.Translation.x, camera.Translation.y, Mathf.Max(camera.Translation.z - (0.05f * Mathf.Abs(Translation.z + 1.5f - camera.Translation.z)), Translation.z + 1.5f));
+	}
+}
